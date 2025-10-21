4153752caf571dbcb72ad44379849a76f16b25ef
RuttiTuttii <61704909+RuttiTuttii@users.noreply.github.com>
2025-10-21 11:00:07 +0300

    44444444444

--- PasswordValidator/obj/Debug/net8.0/PasswordValidator.AssemblyInfo.cs
+++ PasswordValidator/obj/Debug/net8.0/PasswordValidator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce13200d0aadf7f593758c9943e818e84015cb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
